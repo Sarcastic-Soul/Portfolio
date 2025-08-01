80a7a46b868d8945ced3503e704eeba7a17b092e
Sarcastic-Soul <anishisbusy@gmail.com>
2025-08-01 20:18:22 +0530

    add literal bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  // basePath: "/Portfolio", // Uncomment for production deployment
+  basePath: isProd ? "/Portfolio" : "",
   output: "export",
   distDir: "out",
   reactStrictMode: true,
